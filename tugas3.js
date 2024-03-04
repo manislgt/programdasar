79235c36cd8e4c5dcc8780040896267c3aa94364
nye <94160849+manislgt@users.noreply.github.com>
2024-03-04 21:09:50 +0700

    Add files via upload

---
+++ tugas3.js
@@ -0,0 +1,55 @@
+// Ini comment satu baris
+/* Tipe data di JavaScript adalah:
+1. String (teks)
+2. Integer atau Number (bilangan bulat)
+3. Float (bilangan pecahan)
+4. Boolean
+5. Object
+*/
+
+//Tipe data string di JavaScript
+const namaDepan = "Manis"
+const namaBelakang = "Legit"
+let alamat = "Jalan Mangga Berhantu"
+
+//Tipe data integer di Javascript
+let umur = 23
+let noHP = 0811111111111
+let noRumah = 29
+
+//Tipe data float di Javascript
+let beratBadan = 45.5
+let tinggiBadan = 200.5
+let ukuranSepatu = 49.5
+
+//Tipe data boolean di Javasript
+let x = 5
+let y = 5
+let z = 6
+console.log(x == y) //true
+console.log(x == z) //false
+
+//Tipe data object di Javascript
+const nama = { firstName: "Manis", lastName: "Legit" }
+let alamatRumah = { jalan: "Jalan Mangga Berhantu", kota: "Solo" }
+let hobi = { hobi1: "Memancing", hobi2: "Melukis" }
+
+//NOMOR 3
+const angkaPertama = 20
+const angkaKedua = 8
+let total
+
+//Penjumlahan
+total = angkaPertama + angkaKedua
+console.log(total)
+//Pengurangan
+total2 = angkaPertama - angkaKedua
+console.log(total2)
+
+//Perkalian
+total3 = angkaPertama * angkaKedua
+console.log(total3)
+
+//Pembagian
+total4 = angkaPertama / angkaKedua
+console.log(total4)
